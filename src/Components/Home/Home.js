a63983ea6a1c722a3799e57da23c42b8b35d0145
javierodst <javierodst@hotmail.com>
2020-05-16 09:06:29 -0400

    Initial Commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Home extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+
+                <h1>Home</h1>
+
+            </div>
+        );
+    }
+
+}
+
+export default withRouter(Home);
