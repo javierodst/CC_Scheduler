982514d680797e645229ab53979b8fa4ee1e993a
javierodst <javierodst@hotmail.com>
2020-06-20 00:28:47 -0400

    Changed Employee Edit

--- src/Components/Employee/EmployeeEdit.js
+++ src/Components/Employee/EmployeeEdit.js
@@ -41,7 +41,41 @@ class EmployeeEdit extends React.Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
-    handleOnSubmit = () => {
+    handleOnSubmit = (e) => {
+
+        let emp = this.state.empItem;
+        let editedEmp = {
+            '_id': this.props.id, 'name': emp.name, 'employeeId': emp.employeeId, 'email': emp.email, 'branch': emp.branch, 'position': emp.position,
+            'rank': emp.rank, 'status': emp.status, 'totalHours': emp.totalHours, 'weekId': emp.weekId, 'weekSchedule': emp.weekSchedule
+        };
+
+        fetch(this.urlEmp, {
+            method: 'PUT',
+            header: { "Content-Type": 'application/json' },
+            body: JSON.stringify(editedEmp)
+        })
+            .then((response) => {
+
+                if (response.ok) {
+                    console.log("OK");
+                    return response.json();
+                }
+
+                else if (response.status >= 400 && response.status <= 500) {
+                    throw Error(`HTTP ${response.status}, ${response.statusText}`);
+                }
+
+                else {
+                    throw Error(`HTTP ${response.status}, ${response.statusText}`);
+                }
+
+            })
+            .then(responseData => {
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                console.log(error);
+            })
 
     }
 
@@ -73,7 +107,7 @@ class EmployeeEdit extends React.Component {
 
                             <label className='control-label col-md-2'>Employee ID</label>
                             <div className="col-md-6">
-                                <input name="employeeId" className="form-control" ref={(i) => { this.input = i }} onChange={this.handleNameChange} value={this.state.empItem.employeeId} />
+                                <input name="employeeId" className="form-control" ref={(i) => { this.input = i }} onChange={this.handleNameChange} placeholder={this.state.empItem.employeeId} />
                             </div>
 
 
@@ -95,6 +129,13 @@ class EmployeeEdit extends React.Component {
                             <label className='control-label col-md-2'>Rank</label>
                             <div className="col-md-6">
                                 <input name="rank" className="form-control" ref={(i) => { this.input = i }} onChange={this.handleNameChange} value={this.state.empItem.rank} />
+                            </div>
+
+                            <div className="form-group">
+                                <div className="col-md-offset-2 col-md-6">
+                                    <button onClick={this.handleOnSubmit} className="btn btn-primary">Save</button>
+                                    <Link className='btn btn-default' to='/'>Cancel</Link>
+                                </div>
                             </div>
                         </div>
 
