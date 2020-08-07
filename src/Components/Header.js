3d77f91813ff0181c2cd66a0e88195b7251b0d4e
javierodst <javierodst@hotmail.com>
2020-08-07 17:36:59 -0400

    Updated header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,39 +2,62 @@ import React from 'react';
 import auth from '../auth'
 import { Link, Route, Router, withRouter } from 'react-router-dom';
 
-const Header = (props) => {
-    if (auth.isAuthenticated()) {
-        return (
+class Header extends React.Component {
 
-            <div>
-                <div className="header-body">
+    constructor(props) {
+        super(props);
+    }
 
-                    <div className="header-nav">
-                        <ul>
-                            <li><Link className="links" to="/home">Home</Link></li>
-                            <li><Link className="links" to="/report">Report</Link></li>
-                            <li><Link className="links" to="/video">Video</Link></li>
-                            <li><Link className="links" onClick={() => {
-                                auth.logout(() => {
-                                    props.history.push("/");
-                                });
-                            }}
-                            >Logout</Link></li>
-                        </ul>
+    componentDidMount() {
+        $(document).ready(function () {
+            $(".burger-nav").on("click", function () {
+                $(".header-nav ul").toggleClass("open");
+            });
+
+            $(".header-nav ul li .links").on("click", function () {
+                $(".header-nav ul").toggleClass("open");
+            });
+        });
+    }
+
+    header = () => {
+        if (auth.isAuthenticated()) {
+            return (
+
+                <div>
+                    <div className="header-body">
+
+                        <div className="header-nav">
+                            <ul>
+                                <li><Link className="links" to="/home">Home</Link></li>
+                                <li><Link className="links" to="/report">Report</Link></li>
+                                <li><Link className="links" to="/video">Video</Link></li>
+                                <li><Link className="links" onClick={() => {
+                                    auth.logout(() => {
+                                        props.history.push("/");
+                                    });
+                                }}
+                                >Logout</Link></li>
+                            </ul>
+
+                        </div>
 
                     </div>
 
                 </div>
+            );
+        }
 
-            </div>
-        );
+        else {
+            return (
+                <div></div>
+            )
+        }
+    }
+    render() {
+        return ({ header })
     }
 
-    else {
-        return (
-            <div></div>
-        )
-    }
 };
 
 export default withRouter(Header);
